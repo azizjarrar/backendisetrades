9c68c57d6964480e7bc51567598ddadb9daeabb4
unknown <tripelbil@gmail.com>
2021-07-22 22:32:09 +0100

    gzeg

--- api/routes/evenementiel/participation.js
+++ api/routes/evenementiel/participation.js
@@ -6,5 +6,6 @@ const checkauth_event = require('../../middleware/check_auth_evenmentiel')
 router.post('/addParticipation', checkauth_event,participation_controler.addParticipation)
 router.post('/getAllParticipation', checkauth_event,participation_controler.getAllParticipation)
 router.post('/updatestatut', checkauth_event,participation_controler.updatestatut)
+router.post('/deleteParticipation', checkauth_event,participation_controler.deleteParticipation)
 
 module.exports = router

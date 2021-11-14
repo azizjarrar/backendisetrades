0720fe56199ab8eea96e78fee0bd1fd77d333dc0
hayet <35689010+hayetbh@users.noreply.github.com>
2021-11-14 21:03:08 +0100

    cypress

--- db_connection.js
+++ db_connection.js
@@ -5,17 +5,16 @@
     user: "5mrruYpkTT",
     password: "MbMXb71BlA",
     database: "5mrruYpkTT"
-    */
+    *//*
        host: "remotemysql.com",
     user: "XQp55NTp8q",
     password: "Fls7d6vavp",
     database: "XQp55NTp8q"
-    
-    /*
+*/
 
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'club'
-    */
+    database: 'celva'
+    
 });

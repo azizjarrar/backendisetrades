4759dd49cce0a01bef855d8ab46b4d05063fe699
aziz jarrar <tripelbil@gmail.com>
2021-11-19 19:29:43 +0100

    fix

--- db_connection.js
+++ db_connection.js
@@ -1,9 +1,9 @@
  module.exports = require('mysql').createConnection({
-    /* host: "remotemysql.com",
+    /*host: "remotemysql.com",
     user: "5mrruYpkTT",
     password: "MbMXb71BlA",
-    database: "5mrruYpkTT"
-    */
+    database: "5mrruYpkTT"*/
+    
     //    host: "localhost",
     // user: "root",
     // password: "",
@@ -15,9 +15,9 @@
     user: 'root',
     password: '',
     database: 'club'
-    */
+    
     host: "localhost",
     user: "root",
     password: '',
-    database: "5mrruypktt"
+    database: "5mrruypktt"*/
 });

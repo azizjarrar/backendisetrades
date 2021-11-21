9e646dd956af2f1b9b11fc83f6ee38afbbacbaf4
hayet <35689010+hayetbh@users.noreply.github.com>
2021-11-21 10:45:51 +0100

    no

--- db_connection.js
+++ db_connection.js
@@ -5,16 +5,16 @@
     user: "5mrruYpkTT",
     password: "MbMXb71BlA",
     database: "5mrruYpkTT"
-    */
+    *//*
        host: "remotemysql.com",
     user: "XQp55NTp8q",
     password: "Fls7d6vavp",
     database: "XQp55NTp8q"
+     */
 
-/*
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'club'
-    */
+    database: 'celva'
+    
 });
